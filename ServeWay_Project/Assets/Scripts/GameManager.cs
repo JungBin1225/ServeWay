f0e6cc52e0be247167cff7896d48c8c496698e0e
smatphone <jb5982@naver.com>
2023-05-30 16:06:08 +0900

    Create Project
    1. 프로젝트 생성
    2. 작업에 필요한 폴더 생성
    3. 기본 설정

---
+++ ServeWay_Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    private void Awake()
+    {
+        if (gameManager == null)
+            gameManager = this;
+
+        else if (gameManager != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
