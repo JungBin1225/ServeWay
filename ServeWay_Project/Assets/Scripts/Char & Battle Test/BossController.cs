049c80528a3b9567358c2a385be747178670fee7
smatphone <jb5982@naver.com>
2023-08-22 14:43:22 +0900

    적, 보스 충돌방식 변경

--- ServeWay_Project/Assets/Scripts/Char & Battle Test/BossController.cs
+++ ServeWay_Project/Assets/Scripts/Char & Battle Test/BossController.cs
@@ -236,9 +236,9 @@ public class BossController : MonoBehaviour
         StartCoroutine(EnemyMove());
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag != "Player")
+        if (collision.gameObject.tag == "Wall")
         {
             rigidbody.velocity *= -1;
         }
