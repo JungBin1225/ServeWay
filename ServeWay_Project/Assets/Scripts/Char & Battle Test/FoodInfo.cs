a2f75808123d0fd02f6f9e854b4248510b3b5e47
smatphone <jb5982@naver.com>
2023-10-16 14:39:33 +0900

    재료, 음식의 세부 데이터 추가

--- ServeWay_Project/Assets/Scripts/Char & Battle Test/FoodInfo.cs
+++ ServeWay_Project/Assets/Scripts/Char & Battle Test/FoodInfo.cs
@@ -3,6 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public enum Food_Grade
+{
+    STAR_1,
+    STAR_2,
+    STAR_3,
+    STAR_4
+};
+
+public enum Food_MainIngred
+{
+    MEAT,
+    RICE,
+    SOUP,
+    NOODLE,
+    BREAD
+};
+
+public enum Food_Nation
+{
+    KOREA,
+    JAPAN,
+    CHINA,
+    USA,
+    FRANCE
+};
+
 [Serializable]
 public class NameAmount : SerializableDictionary<IngredientList.IngredientsName, int> { }
 
@@ -11,6 +37,15 @@ public class FoodInfo
 {
     public string foodName;
     public Sprite foodSprite;
+
+    public Food_Grade grade;
+    public Food_MainIngred mainIngred;
+    public Food_Nation nation;
+
+    public float damage;
+    public float speed;
+    public float coolTime;
+
     public GameObject foodPrefab;
     public NameAmount needIngredient;
 }
