5b746606d6ef55b0575a64a5ea39676b6bc4ba37
smatphone <jb5982@naver.com>
2024-05-02 15:58:13 +0900

    국물 젓기 미니게임 추가

--- ServeWay_Project/Assets/01.Scripts/Char & Battle Test/MiniGame/SoupGame.cs
+++ ServeWay_Project/Assets/01.Scripts/Char & Battle Test/MiniGame/SoupGame.cs
@@ -9,8 +9,13 @@ public class SoupGame : MonoBehaviour
     public GameObject gamePanel;
     public GameObject pot;
     public GameObject spoon;
+    public GameObject nowSpeed;
+    public GameObject targetSpeed;
 
+    private Create_Success success;
     private bool spoonDown;
+    private float time;
+    private float score;
 
     private float minX;
     private float maxX;
@@ -19,7 +24,9 @@ public class SoupGame : MonoBehaviour
 
     private void OnEnable()
     {
+        score = 0;
         spoonDown = false;
+        time = Time.realtimeSinceStartup;
 
         minX = pot.GetComponent<RectTransform>().transform.position.x - (pot.GetComponent<RectTransform>().sizeDelta.x / 2);
         maxX = pot.GetComponent<RectTransform>().transform.position.x + (pot.GetComponent<RectTransform>().sizeDelta.x / 2);
@@ -43,6 +50,93 @@ public class SoupGame : MonoBehaviour
         }
     }
 
+    public IEnumerator GameStart()
+    {
+        explanePanel.SetActive(false);
+        gamePanel.SetActive(true);
+
+        float now = Time.realtimeSinceStartup - time;
+        Vector3 prevLocation = spoon.GetComponent<RectTransform>().transform.position;
+        float degree = 0;
+        float zeroCount = 0;
+        float targetDegree = Random.Range(3.0f, 5.5f);
+        List<float> degreeList = new List<float>();
+
+        targetSpeed.GetComponent<RectTransform>().anchoredPosition = new Vector3(-200 + (targetDegree * 40), 300, 0);
+
+        while ((Time.realtimeSinceStartup - time) - now < 40)
+        {
+            degreeList.Add((prevLocation - spoon.GetComponent<RectTransform>().transform.position).magnitude);
+            if(degreeList.Count == 5)
+            {
+                if(degreeList.TrueForAll(isZero))
+                {
+                    degree = 0;
+                }
+                else
+                {
+                    foreach(float num in degreeList)
+                    {
+                        if(num != 0)
+                        {
+                            degree += num;
+                            zeroCount++;
+                        }
+                    }
+
+                    degree /= zeroCount;
+                }
+
+                nowSpeed.GetComponent<RectTransform>().anchoredPosition = new Vector3(-200 + (degree * 40), 300, 0);
+                if(degree <= targetDegree - 1.5f)
+                {
+                    Debug.Log("Late");
+                    //타는 이펙트
+                }
+                else if(degree >= targetDegree + 1.5f)
+                {
+                    Debug.Log("Fast");
+                    //튀는 이펙트
+                }
+                else
+                {
+                    score++;
+                    Debug.Log(score);
+                }
+
+                degreeList.Clear();
+                zeroCount = 0;
+                degree = 0;
+            }
+            
+            prevLocation = spoon.GetComponent<RectTransform>().transform.position;
+            yield return null;
+        }
+
+        if (score >= 1200)
+        {
+            success = Create_Success.GREAT;
+        }
+        else if (score >= 800)
+        {
+            success = Create_Success.SUCCESS;
+        }
+        else
+        {
+            success = Create_Success.FAIL;
+        }
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        createUI.success = success;
+        createUI.OnGameCleared(this.gameObject);
+    }
+
+    private bool isZero(float num)
+    {
+        return num == 0;
+    }
+
     public void OnPointerDown()
     {
         spoonDown = true;
@@ -55,7 +149,7 @@ public class SoupGame : MonoBehaviour
 
     public void OnStartClicked()
     {
-        //StartCoroutine(GameStart());
+        StartCoroutine(GameStart());
     }
 
     public void CloseWindow()
