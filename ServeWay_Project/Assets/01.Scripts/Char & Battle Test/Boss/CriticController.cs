23e64abfc8b212d82999df4901dad77fe9db8f3c
smatphone <jb5982@naver.com>
2024-07-15 16:00:17 +0900

    평론가 패턴 개발 중 2

--- ServeWay_Project/Assets/01.Scripts/Char & Battle Test/Boss/CriticController.cs
+++ ServeWay_Project/Assets/01.Scripts/Char & Battle Test/Boss/CriticController.cs
@@ -12,16 +12,20 @@ public class CriticController : MonoBehaviour
     private Vector2 maxPos;
     private float coolTime;
     private bool isAttack;
+    private bool isCharge;
 
     public int test;
     public BossRoom room;
     public GameObject damageEffect;
+    public GameObject pen;
     public List<GameObject> bulletPrefab;
     public float hp;
     public float speed;
     public float attackCoolTime;
+    public float chargeSpeed;
     public float bulletSpeed;
     public float bulletDamage;
+    public float penDamage;
     public Food_Nation nation;
     public Boss_Job job;
 
@@ -39,6 +43,7 @@ public class CriticController : MonoBehaviour
 
         coolTime = attackCoolTime;
         isAttack = false;
+        isCharge = false;
 
         minPos = new Vector2(room.transform.position.x - (room.GetComponent<BoxCollider2D>().size.x / 2), room.transform.position.y - (room.GetComponent<BoxCollider2D>().size.y / 2));
         maxPos = new Vector2(room.transform.position.x + (room.GetComponent<BoxCollider2D>().size.x / 2), room.transform.position.y + (room.GetComponent<BoxCollider2D>().size.y / 2));
@@ -83,7 +88,7 @@ public class CriticController : MonoBehaviour
                 StartCoroutine(ShotGunPattern());
                 break;
             case 1:
-                
+                StartCoroutine(CutPattern());
                 break;
             case 2:
                 StartCoroutine(MachineGunPattern());
@@ -162,6 +167,48 @@ public class CriticController : MonoBehaviour
             }
             yield return new WaitForSeconds(0.3f);
         }
+        yield return new WaitForSeconds(0.3f);
+
+        isAttack = false;
+        coolTime = attackCoolTime;
+        StartCoroutine(EnemyMove());
+    }
+
+    private IEnumerator CutPattern()
+    {
+        isAttack = true;
+        yield return new WaitForSeconds(0.35f);
+
+        isCharge = true;
+        Vector3 target = player.transform.position;
+        Vector2 velo = new Vector2(target.x - transform.position.x, target.y - transform.position.y).normalized * chargeSpeed;
+
+        while(Vector3.Distance(target, transform.position) > 1)
+        {
+            rigidbody.velocity = velo;
+            yield return null;
+        }
+
+        rigidbody.velocity = new Vector2(0, 0);
+        isCharge = false;
+        yield return new WaitForSeconds(0.1f);
+
+        Vector2 direction = player.transform.position - transform.position;
+        Vector3 startRot = Quaternion.FromToRotation(Vector3.down, direction).eulerAngles;
+        int num = 0;
+
+        pen.SetActive(true);
+        pen.transform.GetChild(0).GetComponent<Pen>().damage = penDamage;
+        pen.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, startRot.z - 25));
+
+        while(num < 65)
+        {
+            pen.transform.localRotation = Quaternion.Euler(pen.transform.localRotation.eulerAngles + new Vector3(0, 0, 2));
+            num++;
+            yield return null;
+        }
+
+        pen.SetActive(false);
         yield return new WaitForSeconds(0.3f);
 
         isAttack = false;
