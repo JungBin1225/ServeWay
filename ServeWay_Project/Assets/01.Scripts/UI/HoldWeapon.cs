2de318dd8722701318d49f533e7cecc15543a361
smatphone <jb5982@naver.com>
2024-12-03 16:03:45 +0900

    현재 음식 & 대시 쿨타임 UI 변경

--- ServeWay_Project/Assets/01.Scripts/UI/HoldWeapon.cs
+++ ServeWay_Project/Assets/01.Scripts/UI/HoldWeapon.cs
@@ -20,19 +20,21 @@ public class HoldWeapon : MonoBehaviour
 
     public void UpdateHoldWeapon(FoodData food)
     {
+        holdWeapon.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 1);
         holdWeapon.transform.GetChild(1).GetComponent<Image>().sprite = food.foodSprite;
         holdWeapon.transform.GetChild(2).GetComponent<Text>().text = food.foodName;
     }
 
     public void UpdateHoldWeapon_Null()
     {
-        holdWeapon.transform.GetChild(1).GetComponent<Image>().sprite = null;
+        holdWeapon.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 0);
         holdWeapon.transform.GetChild(2).GetComponent<Text>().text = "";
     }
 
     public IEnumerator ChargeCoolTime(float time)
     {
         chargeCoolTime.SetActive(true);
+        chargeCoolTime.GetComponent<Image>().fillAmount = 1;
         chargeCoolTime.transform.GetChild(0).GetComponent<Text>().text = time.ToString("F1");
 
         float timeTemp = time;
@@ -40,6 +42,7 @@ public class HoldWeapon : MonoBehaviour
         {
             timeTemp -= Time.deltaTime;
             chargeCoolTime.transform.GetChild(0).GetComponent<Text>().text = timeTemp.ToString("F1");
+            chargeCoolTime.GetComponent<Image>().fillAmount = (timeTemp / time);
             yield return null;
         }
         chargeCoolTime.transform.GetChild(0).GetComponent<Text>().text = "0.0";
