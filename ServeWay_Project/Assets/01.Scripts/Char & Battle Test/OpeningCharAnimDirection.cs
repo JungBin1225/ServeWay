b2ee5bf05f737226af871c2b621dcf7d661e6323
JungBin1225 <jb5982@naver.com>
2025-05-21 23:07:47 +0900

    오프닝 씬 캐릭터 방향 지정

---
+++ ServeWay_Project/Assets/01.Scripts/Char & Battle Test/OpeningCharAnimDirection.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningCharAnimDirection : MonoBehaviour
+{
+    public string direction;
+
+    private Animator anim;
+    private Vector3 pos;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        pos = transform.position;
+
+        SetDir(direction);
+    }
+
+    private void OnEnable()
+    {
+        anim = GetComponent<Animator>();
+        pos = transform.position;
+
+        SetDir(direction);
+    }
+
+    void Update()
+    {
+        if(direction == "walk" || direction == "stop")
+        {
+            if (pos != transform.position)
+            {
+                anim.SetBool("walk", true);
+            }
+            else
+            {
+                anim.SetBool("walk", false);
+            }
+
+            pos = transform.position;
+        }
+
+        if(gameObject.name.Contains("Player") && direction == "back")
+        {
+            if (pos != transform.position)
+            {
+                anim.SetBool("side", false);
+                anim.SetBool("front", false);
+                anim.SetBool("back", true);
+            }
+            else
+            {
+                anim.SetBool("back", false);
+                anim.SetBool("front", false);
+                anim.SetBool("side", true);
+            }
+
+            pos = transform.position;
+        }
+    }
+
+    public void SetDir(string direction)
+    {
+        switch (direction)
+        {
+            case "front":
+                anim.SetBool("back", false);
+                anim.SetBool("side", false);
+                anim.SetBool("front", true);
+                break;
+            case "back":
+                anim.SetBool("side", false);
+                anim.SetBool("front", false);
+                anim.SetBool("back", true);
+                break;
+            case "side":
+                anim.SetBool("back", false);
+                anim.SetBool("front", false);
+                anim.SetBool("side", true);
+                break;
+
+            case "walk":
+                anim.SetBool("walk", true);
+                break;
+            case "stop":
+                anim.SetBool("walk", false);
+                break;
+        }
+    }
+}
