1d69482920e7b062b27d749d5ed039696a75e506
JungBin1225 <jb5982@naver.com>
2025-05-29 23:07:02 +0900

    Hp 이미지 스프라이트 변경, 애니메이션 추가

--- ServeWay_Project/Assets/01.Scripts/UI/HPBar.cs
+++ ServeWay_Project/Assets/01.Scripts/UI/HPBar.cs
@@ -7,6 +7,7 @@ public class HPBar : MonoBehaviour
 {
     private PlayerHealth playerHealth;
     private List<Image> hpImages;
+    private List<Animator> hpAnim;
 
     [SerializeField] Sprite empty;
     [SerializeField] Sprite full;
@@ -18,10 +19,19 @@ public class HPBar : MonoBehaviour
         playerHealth = GameObject.Find("Player").GetComponent<PlayerHealth>();
 
         hpImages = new List<Image>();
+        hpAnim = new List<Animator>();
+
         for(int i = 0; i < transform.childCount; i++)
         {
             hpImages.Add(transform.GetChild(i).GetComponent<Image>());
         }
+
+        foreach(Image image in hpImages)
+        {
+            hpAnim.Add(image.gameObject.GetComponent<Animator>());
+        }
+
+        StartCoroutine(HpIdleAnim());
     }
 
     // Update is called once per frame
@@ -40,11 +50,17 @@ public class HPBar : MonoBehaviour
             {
                 if(i < hp / 2)
                 {
-                    hpImages[i].sprite = full;
+                    if(hpImages[i].sprite != full)
+                    {
+                        PlayHpAnim(i, 2);
+                    }
                 }
                 else
                 {
-                    hpImages[i].sprite = empty;
+                    if (hpImages[i].sprite != empty)
+                    {
+                        PlayHpAnim(i, 0);
+                    }
                 }
             }
         }
@@ -54,17 +70,48 @@ public class HPBar : MonoBehaviour
             {
                 if (i < hp / 2)
                 {
-                    hpImages[i].sprite = full;
+                    if (hpImages[i].sprite != full)
+                    {
+                        PlayHpAnim(i, 2);
+                    }
                 }
                 else if(i < (hp + 1) / 2)
                 {
-                    hpImages[i].sprite = half;
+                    if (hpImages[i].sprite != half)
+                    {
+                        PlayHpAnim(i, 1);
+                    }
                 }
                 else
                 {
-                    hpImages[i].sprite = empty;
+                    if (hpImages[i].sprite != empty)
+                    {
+                        PlayHpAnim(i, 0);
+                    }
                 }
             }
         }
+    }
+
+    private IEnumerator HpIdleAnim()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(2);
+            foreach(Animator anim in hpAnim)
+            {
+                if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name.Contains("Idle") || anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+                {
+                    anim.SetTrigger("idle");
+                }
+                
+            }
+        }
+    }
+
+    private void PlayHpAnim(int index, int state)
+    {
+        hpAnim[index].SetInteger("state", state);
+        hpAnim[index].SetTrigger("change");
     }
 }
