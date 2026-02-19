569eabf81d44c22439e3abd7edf65c0755d13c13
JungBin1225 <jb5982@naver.com>
2026-02-19 22:55:33 +0900

    유튜버 보스 이펙트 추가
    1. 알고리즘 패턴을 마우스로 클릭하는 이펙트 추가

--- ServeWay_Project/Assets/01.Scripts/Char & Battle/Boss/Algorithm.cs
+++ ServeWay_Project/Assets/01.Scripts/Char & Battle/Boss/Algorithm.cs
@@ -6,7 +6,9 @@ public class Algorithm : MonoBehaviour
 {
     private GameObject effectParent;
     private SpriteRenderer spriteRenderer;
+    private bool touched;
 
+    public GameObject cursor;
     public Vector3 target;
     public float speed;
     public float damage;
@@ -21,13 +23,18 @@ public class Algorithm : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = food;
         effectParent = GameObject.Find("EffectList");
+        touched = false;
 
         spriteRenderer.sprite = sprites[Random.Range(0, sprites.Count)];
     }
 
     void Update()
     {
-        Fire();
+        if(!touched)
+        {
+            Fire();
+        }
+
 
         if(boss == null)
         {
@@ -42,9 +49,29 @@ public class Algorithm : MonoBehaviour
         transform.position -= dir.normalized * Time.deltaTime * speed;
     }
 
+    private IEnumerator DestroyBullet()
+    {
+        cursor.SetActive(true);
+        cursor.transform.localRotation = Quaternion.Euler(0, 0, -transform.rotation.eulerAngles.z);
+
+        yield return new WaitForSeconds(0.2f);
+
+        Vector3 dir = new Vector3(target.x, target.y, 0);
+        float dis = transform.localScale.x;
+
+        while (transform.localScale.x > 0.01f)
+        {
+            transform.localScale = transform.localScale - new Vector3(dis * Time.deltaTime * speed * 1.5f, dis * Time.deltaTime * speed * 1.5f, 0);
+            transform.position -= dir.normalized * Time.deltaTime * speed * 1.5f;
+            yield return null;
+        }
+
+        Destroy(this.gameObject);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" && !touched)
         {
             List<Sprite> sprites = new List<Sprite>();
             sprites.Add(sprite);
@@ -55,8 +82,9 @@ public class Algorithm : MonoBehaviour
 
         if(collision.gameObject.tag == "Boss")
         {
+            touched = true;
             Instantiate(sound, transform.position, Quaternion.Euler(0, 0, 0), effectParent.transform);
-            Destroy(this.gameObject);
+            StartCoroutine(DestroyBullet());
         }
     }
 }
